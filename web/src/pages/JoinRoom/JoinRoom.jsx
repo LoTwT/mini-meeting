28d785cd0c70444f137450dff138f24c455d2ed0
LoTwT <709937065@qq.com>
2021-12-25 00:53:31 +0800

    feat(JoinRoom)

--- web/src/pages/JoinRoom/JoinRoom.jsx
+++ web/src/pages/JoinRoom/JoinRoom.jsx
@@ -1,7 +1,22 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { useLocation } from "react-router-dom"
 
-const JoinRoom = () => {
-  return <div>JoinRoom</div>
+const JoinRoom = (props) => {
+  const search = useLocation().search
+
+  useEffect(() => {
+    const isRoomHost = new URLSearchParams(search).get("host")
+
+    if (isRoomHost) {
+      // 将主持人的状态保存到 redux 的 store 里
+    }
+  })
+
+  return (
+    <div className="join_room_page_container">
+      <div className="join_room_page_panel"></div>
+    </div>
+  )
 }
 
 export default JoinRoom
