5c0b1778ecda5ba87ae0d399f6fafcf8459114b5
LoTwT <709937065@qq.com>
2021-12-25 00:44:21 +0800

    feat(Introduction)

--- web/src/pages/Introduction/Introduction.jsx
+++ web/src/pages/Introduction/Introduction.jsx
@@ -1,7 +1,18 @@
 import React from "react"
 
+import "./Introduction.css"
+import logo from "../../resources/images/logo.png"
+import ConnectingButtons from "./components/ConnectingButtons"
+
 const Introduction = () => {
-  return <div>introduction</div>
+  return (
+    <div className="introduction_page_container">
+      <div className="introduction_page_panel">
+        <img className="introduction_page_image" src={logo} alt="logo" />
+        <ConnectingButtons />
+      </div>
+    </div>
+  )
 }
 
 export default Introduction
