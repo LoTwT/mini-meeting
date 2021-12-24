ae0cd00f1a414fd958e5d3b2e98c03ff68eb7678
LoTwT <709937065@qq.com>
2021-12-25 00:16:33 +0800

    feat(Router): add react-router-dom

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,19 @@
 import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Introduction from "./pages/Introduction/Introduction.jsx"
+import JoinRoom from "./pages/JoinRoom/JoinRoom.jsx"
+import Room from "./pages/Room/Room.jsx"
 
 function App() {
-  return <div className="App">app</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Introduction />} />
+        <Route path="/join-room" element={<JoinRoom />} />
+        <Route path="/room" element={<Room />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
