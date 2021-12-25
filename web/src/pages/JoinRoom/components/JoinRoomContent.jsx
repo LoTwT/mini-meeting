6ebbf343401957031b54a691d12cf8171e9caaaa
LoTwT <709937065@qq.com>
2021-12-25 17:32:52 +0800

    feat(JoinRoom)

--- web/src/pages/JoinRoom/components/JoinRoomContent.jsx
+++ web/src/pages/JoinRoom/components/JoinRoomContent.jsx
@@ -5,12 +5,18 @@ import JoinRoomInputs from "./JoinRoomInputs"
 import OnlyWithAudioCheckbox from "./OnlyWithAudioCheckbox"
 import { setConnectOnlyWithAudio } from "../../../store/action"
 import ErrorMessage from "./ErrorMessage"
+import JoinRoomButtons from "./JoinRoomButtons"
 
 const JoinRoomContent = (props) => {
   const { isRoomHost, setConnectOnlyWithAudio, connectOnlyWithAudio } = props
 
   const [roomIdValue, setRoomIdValue] = useState("")
   const [nameValue, setNameValue] = useState("")
+
+  // 加入房间
+  const handleJoinRoom = () => {
+    console.log("加入房间成功")
+  }
 
   return (
     <>
@@ -26,6 +32,10 @@ const JoinRoomContent = (props) => {
         setConnectOnlyWithAudio={setConnectOnlyWithAudio}
       />
       <ErrorMessage errorMessage={"会议 ID 不存在！"} />
+      <JoinRoomButtons
+        isRoomHost={isRoomHost}
+        handleJoinRoom={handleJoinRoom}
+      />
     </>
   )
 }
