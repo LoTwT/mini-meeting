d84b8b086f16bb809ed53f58d76937363cdea0f8
LoTwT <709937065@qq.com>
2021-12-25 18:16:26 +0800

    feat(JoinRoom): create & join room

--- server/index.js
+++ server/index.js
@@ -11,6 +11,35 @@ const server = http.createServer(app)
 // 处理跨域
 app.use(cors())
 
+// ===========================================================================
+
+// 初始化房间和用户
+const connectedUsers = []
+const rooms = []
+
+// 创建路由验证房间是否存在
+app.get("/api/room-exists/:roomId", (req, res) => {
+  const { roomId } = req.params
+  const isRoomExist = rooms.find((room) => room.id === roomId)
+
+  // 房间存在
+  if (isRoomExist) {
+    // 房间是否满员
+    if (room.connectedUsers.length > 3) {
+      // 房间人数已满
+      return res.send({ roomExists: true, full: true })
+    } else {
+      // 房间可以加入
+      return res.send({ roomExists: true, full: false })
+    }
+  } else {
+    // 房间不存在
+    return res.send({ roomExists: false })
+  }
+})
+
+// ===========================================================================
+
 // socker.io
 const io = require("socket.io")(server, {
   cors: {
--- web/src/pages/JoinRoom/components/JoinRoomContent.jsx
+++ web/src/pages/JoinRoom/components/JoinRoomContent.jsx
@@ -1,21 +1,53 @@
 import React, { useState } from "react"
 import { connect } from "react-redux"
+import { useNavigate } from "react-router-dom"
 
 import JoinRoomInputs from "./JoinRoomInputs"
 import OnlyWithAudioCheckbox from "./OnlyWithAudioCheckbox"
 import { setConnectOnlyWithAudio } from "../../../store/action"
 import ErrorMessage from "./ErrorMessage"
 import JoinRoomButtons from "./JoinRoomButtons"
+import { getRoomExists } from "../../../utils/api"
 
 const JoinRoomContent = (props) => {
+  const navigate = useNavigate()
+
   const { isRoomHost, setConnectOnlyWithAudio, connectOnlyWithAudio } = props
 
   const [roomIdValue, setRoomIdValue] = useState("")
   const [nameValue, setNameValue] = useState("")
+  const [errorMessage, setErrorMessage] = useState(null)
 
-  // 加入房间
-  const handleJoinRoom = () => {
-    console.log("加入房间成功")
+  // 创建 / 加入房间
+  const handleJoinRoom = async () => {
+    if (isRoomHost) {
+      createRoom()
+    } else {
+      await joinRoom()
+    }
+  }
+
+  const joinRoom = async () => {
+    const responseData = await getRoomExists(roomIdValue)
+
+    const { roomExists, full } = responseData
+
+    // 房间是否存在
+    if (roomExists) {
+      // 房间是否满员
+      if (full) {
+        setErrorMessage("会议房间人数已满，请稍后再试！")
+      } else {
+        // 进入房间
+        navigate("/room")
+      }
+    } else {
+      setErrorMessage("会议房间不存在，请验证您的房间 ID 是否正确！")
+    }
+  }
+
+  const createRoom = async () => {
+    navigate("/room")
   }
 
   return (
@@ -31,7 +63,7 @@ const JoinRoomContent = (props) => {
         connectOnlyWithAudio={connectOnlyWithAudio}
         setConnectOnlyWithAudio={setConnectOnlyWithAudio}
       />
-      <ErrorMessage errorMessage={"会议 ID 不存在！"} />
+      <ErrorMessage errorMessage={errorMessage} />
       <JoinRoomButtons
         isRoomHost={isRoomHost}
         handleJoinRoom={handleJoinRoom}
