e64b1e2bf68f889e137ab7e20b440b76d8997f6a
LoTwT <709937065@qq.com>
2021-12-26 15:30:21 +0800

    feat(socket): leave room

--- server/index.js
+++ server/index.js
@@ -56,6 +56,9 @@ io.on("connection", (socket) => {
 
   // 加入会议房间
   socket.on("join-room", (data) => joinRoomHandler(socket, data))
+
+  // 用户断开连接
+  socket.on("disconnect", () => disconnectHandler(socket))
 })
 
 // socket.io handler
@@ -119,6 +122,33 @@ const joinRoomHandler = (socket, data) => {
 
   // 发送通知告知有新用户加入并更新房间
   io.to(roomId).emit("room-update", { connectedUsers: room.connectedUsers })
+}
+
+const disconnectHandler = (socket) => {
+  // 查询要离开会议房间的用户
+  const user = connectedUsers.find((user) => user.socketId === socket.id)
+
+  if (user) {
+    // 从会议房间进行删除
+    const room = rooms.find((room) => room.id === user.roomId)
+
+    room.connectedUsers = room.connectedUsers.filter(
+      (user) => user.socketId !== socket.id,
+    )
+
+    // 离开房间
+    socket.leave(user.roomId)
+
+    if (room.connectedUsers.length > 0) {
+      // 发送通知告知有用户离开并更新房间
+      io.to(room.id).emit("room-update", {
+        connectedUsers: room.connectedUsers,
+      })
+    } else {
+      // 当会议房间没有人员的时候要关闭整个会议房间 (从 rooms 中删除)
+      rooms = rooms.filter((r) => r.id !== room.id)
+    }
+  }
 }
 
 // ===========================================================================
