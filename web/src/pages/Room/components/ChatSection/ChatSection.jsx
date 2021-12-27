25591a67c43e4af1f06497d9e16811fec15aa374
LoTwT <709937065@qq.com>
2021-12-27 22:09:40 +0800

    feat(Room): ChatSection input

--- web/src/pages/Room/components/ChatSection/ChatSection.jsx
+++ web/src/pages/Room/components/ChatSection/ChatSection.jsx
@@ -2,12 +2,14 @@ import React from "react"
 
 import ChatLabel from "./components/ChatLabel"
 import Messages from "./components/Messages"
+import NewMessage from "./components/NewMessage"
 
 const ChatSection = () => {
   return (
     <div className="chat_section_container">
       <ChatLabel />
       <Messages />
+      <NewMessage />
     </div>
   )
 }
